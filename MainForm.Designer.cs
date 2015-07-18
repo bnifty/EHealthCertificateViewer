cc7729b702c9d1e8252af34d3f54b601395386b6
bnifty <github@nifty.be>
2015-07-18 14:26:42 +0200

    initial version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace bnifty.EHealthCertificateViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cmdShow = new System.Windows.Forms.Button();
+            this.cmdZoeken = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            resources.ApplyResources(this.txtPath, "txtPath");
+            this.txtPath.Name = "txtPath";
+            // 
+            // txtPassword
+            // 
+            resources.ApplyResources(this.txtPassword, "txtPassword");
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // cmdShow
+            // 
+            resources.ApplyResources(this.cmdShow, "cmdShow");
+            this.cmdShow.Name = "cmdShow";
+            this.cmdShow.UseVisualStyleBackColor = true;
+            this.cmdShow.Click += new System.EventHandler(this.cmdShow_Click);
+            // 
+            // cmdZoeken
+            // 
+            resources.ApplyResources(this.cmdZoeken, "cmdZoeken");
+            this.cmdZoeken.Name = "cmdZoeken";
+            this.cmdZoeken.UseVisualStyleBackColor = true;
+            this.cmdZoeken.Click += new System.EventHandler(this.cmdZoeken_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdZoeken);
+            this.Controls.Add(this.cmdShow);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button cmdShow;
+        private System.Windows.Forms.Button cmdZoeken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace bnifty.EHealthCertificateViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void cmdZoeken_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.InitialDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"ehealth\keystore");
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPath.Text = dlg.FileName;
+            }
+        }
+
+        private void cmdShow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var cert = new X509Certificate2(txtPath.Text, txtPassword.Text);
+                X509Certificate2UI.DisplayCertificate(cert);
+            }
+            catch
+            {
+                MessageBox.Show(Properties.Resources.MsgErrorReadingFile);
+            }
+        }
+    }
+}
